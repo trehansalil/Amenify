f26dfa920577797e494086c378472f26359e7495
SALIL <salil.trehan@htmedialabs.com>
2022-01-31 16:42:33 +0530

    apartments_crawler.py
    Crawler Dev Commit

--- src/apartment/__init__.py
+++ src/apartment/__init__.py
@@ -82,23 +82,29 @@ class Apartments(scrapy.Spider):
 
     def save_apartments_data_to_db(self, apartments_object):
         mycol = self.apartments_collection
-        if mycol.find({}).count() == 0:
-            print("Inserting new apartment data into ", mycol)
-            mycol.insert_one(apartments_object)
-            print("Inserted new apartment data into ", mycol)
+        # print(mycol.find({}))
+        # if mycol.find({}).count() == 0:
+        #     print("Inserting new apartment data into ", mycol)
+        #     mycol.insert_one(apartments_object)
+        #     print("Inserted new apartment data into ", mycol)
+        try:
+            if mycol.find({"link": apartments_object["link"]}).count() > 0:
+                print("Apartmemts Data Already Exists\n")
+                for x in mycol.find({"link": apartments_object["link"]}):
+                    existing = x
+                    break
 
-        elif mycol.find({"link": apartments_object["link"]}).count() > 0:
-            print("Apartmemts Data Already Exists\n")
-            for x in mycol.find({"link": apartments_object["link"]}):
-                existing = x
-                break
+                print(existing['created_on'], apartments_object['created_on'])
 
-            print(existing['created_on'], apartments_object['created_on'])
-
-            mycol.replace_one({"link": apartments_object["link"]}, existing)
-            print('--------', existing)
-            print("Apartmemts Data Updated\n")
-        else:
+                mycol.replace_one({"link": apartments_object["link"]}, existing)
+                print('--------', existing)
+                print("Apartmemts Data Updated\n")
+            else:
+                print("Inserting new apartment data into ", mycol)
+                mycol.insert_one(apartments_object)
+                print("Inserted new apartment data into ", mycol)
+        except Exception as e:
+            print(f"Insert Error: {e}")
             print("Inserting new apartment data into ", mycol)
             mycol.insert_one(apartments_object)
             print("Inserted new apartment data into ", mycol)
